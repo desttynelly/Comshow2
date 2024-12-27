92135ef120561da34c2242525012237caa7148a3
desttynelly <obhiosad@gmail.com>
2024-12-27 10:03:59 +0100

    first commit

--- assets/js/general.js
+++ assets/js/general.js
@@ -3,3 +3,24 @@
 document.querySelector(".signupb").addEventListener("click", function(){
     window.location = "/signup"
 });
+
+
+
+document.querySelector("#open").addEventListener("click", function(){
+
+    document.querySelector("#open").classList.add("active");
+    document.querySelector("#closee").classList.add("active");
+
+    document.querySelector(".navlinks").classList.add("active");
+
+});
+
+
+document.querySelector("#closee").addEventListener("click", function(){
+
+    document.querySelector("#open").classList.remove("active");
+    document.querySelector("#closee").classList.remove("active");
+
+    document.querySelector(".navlinks").classList.remove("active");
+    
+});
